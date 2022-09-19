55fc711b7d0e4df04f99954e55e7cf61497e58c7
davidmargaryan888 <m.davo888@gmail.com>
2022-09-19 22:48:46 +0400

    has been created to do list app

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('To Do List'),
+        ),
+        body: const Center(
+          child: Text(
+            'To Do List Screen',
+          ),
+        ),
+      ),
+    );
+  }
+}
